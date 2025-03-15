e393a8f8b618a0be4d115e6143bda98e09daf6f7
Timilehin Abodunrin <joeltimmy7@gmail.com>
2025-03-15 21:09:06 +0100

    fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,30 +1,3 @@
-// import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-// import { PrismaClient } from '@prisma/client';
-
-// @Injectable()
-// export class PrismaService
-//   extends PrismaClient
-//   implements OnModuleInit, OnModuleDestroy
-// {
-//   constructor() {
-//     super();
-//   }
-
-//   async onModuleInit() {
-//     await this.$connect();
-//   }
-
-//   async onModuleDestroy() {
-//     await this.$disconnect();
-//   }
-
-//   //   async cleanDatabase() {
-//   // if (process.env.NODE_ENV === 'test') {
-//   // Only allow this in test environment
-//   //   return Promise.all([this.user.deleteMany()]);
-//   // }
-//   //   }
-// }
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
